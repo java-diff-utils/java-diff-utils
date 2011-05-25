28b915519f5d08aaf161fd9f2645c6ba3eb29b2e
dm.naumenko@gmail.com <dm.naumenko@gmail.com@d8d7d024-a22d-11de-b755-fd640f38fa9d>
2011-05-25 11:20:27 +0000

    issue 14: use Integer.valueOf in delta comparison

--- src/difflib/DeltaComparator.java
+++ src/difflib/DeltaComparator.java
@@ -7,13 +7,13 @@ import java.util.Comparator;
  * @author mksenzov
  */
 public class DeltaComparator implements Comparator<Delta>, Serializable {
-
+    private static final long serialVersionUID = 1L;
     public static final Comparator<Delta> INSTANCE = new DeltaComparator();
 
     private DeltaComparator() {
     }
 
     public int compare(final Delta a, final Delta b) {
-        return new Integer(a.getOriginal().getPosition()).compareTo(b.getOriginal().getPosition());
+        return Integer.valueOf(a.getOriginal().getPosition()).compareTo(b.getOriginal().getPosition());
     }
 }
