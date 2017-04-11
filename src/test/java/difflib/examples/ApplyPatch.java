c318530d4280ca01a47442a5262c58e6b9ccfa48
wumpz <t.warneke@gmx.net>
2017-04-11 09:42:43 +0200

    removed abstract example class

--- src/test/java/difflib/examples/ApplyPatch.java
+++ src/test/java/difflib/examples/ApplyPatch.java
@@ -5,16 +5,19 @@ import difflib.TestConstants;
 import difflib.UnifiedDiffUtils;
 import difflib.patch.Patch;
 import difflib.patch.PatchFailedException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
-public class ApplyPatch extends Example {
+public class ApplyPatch {
 
     static final String ORIGINAL = TestConstants.MOCK_FOLDER + "issue10_base.txt";
     static final String PATCH = TestConstants.MOCK_FOLDER + "issue10_patch.txt";
 
-    public static void main(String[] args) throws PatchFailedException {
-        List<String> original = fileToLines(ORIGINAL);
-        List<String> patched = fileToLines(PATCH);
+    public static void main(String[] args) throws PatchFailedException, IOException {
+        List<String> original = Files.readAllLines(new File(ORIGINAL).toPath());
+        List<String> patched = Files.readAllLines(new File(PATCH).toPath());
 
         // At first, parse the unified diff file and get the patch
         Patch<String> patch = UnifiedDiffUtils.parseUnifiedDiff(patched);
--- src/test/java/difflib/examples/ComputeDifference.java
+++ src/test/java/difflib/examples/ComputeDifference.java
@@ -5,16 +5,19 @@ import difflib.TestConstants;
 import difflib.algorithm.DiffException;
 import difflib.patch.Delta;
 import difflib.patch.Patch;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
-public class ComputeDifference extends Example {
+public class ComputeDifference {
 
     static final String ORIGINAL = TestConstants.MOCK_FOLDER + "original.txt";
     static final String RIVISED = TestConstants.MOCK_FOLDER + "revised.txt";
 
-    public static void main(String[] args) throws DiffException {
-        List<String> original = fileToLines(ORIGINAL);
-        List<String> revised = fileToLines(RIVISED);
+    public static void main(String[] args) throws DiffException, IOException {
+        List<String> original = Files.readAllLines(new File(ORIGINAL).toPath());
+        List<String> revised = Files.readAllLines(new File(RIVISED).toPath());
 
         // Compute diff. Get the Patch object. Patch is the container for computed deltas.
         Patch<String> patch = DiffUtils.diff(original, revised);
