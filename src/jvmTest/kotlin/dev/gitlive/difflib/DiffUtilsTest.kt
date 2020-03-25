2547cbfe9f072cf01d6d357e45ef3ba48e307e24
sadevelopment <marcus@squareapple.co.uk>
2020-03-25 12:34:56 +0000

    Rename packages (again) to dev.gitlive

---
+++ src/jvmTest/kotlin/dev/gitlive/difflib/DiffUtilsTest.kt
@@ -0,0 +1,175 @@
+package dev.gitlive.difflib
+
+import dev.gitlive.difflib.algorithm.DiffException
+import dev.gitlive.difflib.patch.ChangeDelta
+import dev.gitlive.difflib.patch.Chunk
+import dev.gitlive.difflib.patch.DeleteDelta
+import dev.gitlive.difflib.patch.AbstractDelta
+import dev.gitlive.difflib.patch.InsertDelta
+import dev.gitlive.difflib.patch.Patch
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
+import java.util.ArrayList
+import java.util.Arrays
+import java.util.Collections
+import java.util.stream.Collectors.toList
+import java.util.zip.ZipFile
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Ignore
+import org.junit.Test
+
+class DiffUtilsTest {
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiff_Insert() {
+        val patch = DiffUtils.diff(Arrays.asList("hhh"), Arrays.asList("hhh", "jjj", "kkk"))
+        assertNotNull(patch)
+        assertEquals(1, patch.deltas.size.toLong())
+        val delta = patch.deltas[0]
+        assertTrue(delta is InsertDelta<*>)
+        assertEquals(Chunk<String>(1, emptyList<String>()), delta.source)
+        assertEquals(Chunk(1, Arrays.asList("jjj", "kkk")), delta.target)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiff_Delete() {
+        val patch = DiffUtils.diff(Arrays.asList("ddd", "fff", "ggg"), Arrays.asList("ggg"))
+        assertNotNull(patch)
+        assertEquals(1, patch.deltas.size.toLong())
+        val delta = patch.deltas[0]
+        assertTrue(delta is DeleteDelta<*>)
+        assertEquals(Chunk(0, Arrays.asList("ddd", "fff")), delta.source)
+        assertEquals(Chunk<String>(0, emptyList<String>()), delta.target)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiff_Change() {
+        val changeTest_from = Arrays.asList("aaa", "bbb", "ccc")
+        val changeTest_to = Arrays.asList("aaa", "zzz", "ccc")
+
+        val patch = DiffUtils.diff(changeTest_from, changeTest_to)
+        assertNotNull(patch)
+        assertEquals(1, patch.deltas.size.toLong())
+        val delta = patch.deltas[0]
+        assertTrue(delta is ChangeDelta<*>)
+        assertEquals(Chunk(1, Arrays.asList("bbb")), delta.source)
+        assertEquals(Chunk(1, Arrays.asList("zzz")), delta.target)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiff_EmptyList() {
+        val patch = DiffUtils.diff(ArrayList(), ArrayList<String>())
+        assertNotNull(patch)
+        assertEquals(0, patch.deltas.size.toLong())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiff_EmptyListWithNonEmpty() {
+        val patch = DiffUtils.diff(ArrayList(), Arrays.asList("aaa"))
+        assertNotNull(patch)
+        assertEquals(1, patch.deltas.size.toLong())
+        val delta = patch.deltas[0]
+        assertTrue(delta is InsertDelta<*>)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiffInline() {
+        val patch = DiffUtils.diffInline("", "test")
+        assertEquals(1, patch.deltas.size.toLong())
+        assertTrue(patch.deltas[0] is InsertDelta<*>)
+        assertEquals(0, patch.deltas[0].source.position.toLong())
+        assertEquals(0, patch.deltas[0].source.lines!!.size.toLong())
+        assertEquals("test", patch.deltas[0].target.lines!![0])
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiffInline2() {
+        val patch = DiffUtils.diffInline("es", "fest")
+        assertEquals(2, patch.deltas.size.toLong())
+        assertTrue(patch.deltas[0] is InsertDelta<*>)
+        assertEquals(0, patch.deltas[0].source.position.toLong())
+        assertEquals(2, patch.deltas[1].source.position.toLong())
+        assertEquals(0, patch.deltas[0].source.lines!!.size.toLong())
+        assertEquals(0, patch.deltas[1].source.lines!!.size.toLong())
+        assertEquals("f", patch.deltas[0].target.lines!![0])
+        assertEquals("t", patch.deltas[1].target.lines!![0])
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiffIntegerList() {
+        val original = Arrays.asList(1, 2, 3, 4, 5)
+        val revised = Arrays.asList(2, 3, 4, 6)
+
+        val patch = DiffUtils.diff(original, revised)
+
+        for (delta in patch.deltas) {
+            println(delta)
+        }
+
+        assertEquals(2, patch.deltas.size.toLong())
+        assertEquals("[DeleteDelta, position: 0, lines: [1]]", patch.deltas[0].toString())
+        assertEquals("[ChangeDelta, position: 4, lines: [5] to [6]]", patch.deltas[1].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiffMissesChangeForkDnaumenkoIssue31() {
+        val original = Arrays.asList("line1", "line2", "line3")
+        val revised = Arrays.asList("line1", "line2-2", "line4")
+
+        val patch = DiffUtils.diff(original, revised)
+        assertEquals(1, patch.deltas.size.toLong())
+        assertEquals("[ChangeDelta, position: 1, lines: [line2, line3] to [line2-2, line4]]", patch.deltas[0].toString())
+    }
+
+    /**
+     * To test this, the greedy meyer algorithm is not suitable.
+     */
+    @Test
+    @Ignore
+    @Throws(IOException::class, DiffException::class)
+    fun testPossibleDiffHangOnLargeDatasetDnaumenkoIssue26() {
+        val zip = ZipFile(TestConstants.MOCK_FOLDER + "/large_dataset1.zip")
+
+        val patch = DiffUtils.diff(
+                readStringListFromInputStream(zip.getInputStream(zip.getEntry("ta"))),
+                readStringListFromInputStream(zip.getInputStream(zip.getEntry("tb"))))
+
+        assertEquals(1, patch.deltas.size.toLong())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testDiffMyersExample1() {
+        val patch = DiffUtils.diff(Arrays.asList("A", "B", "C", "A", "B", "B", "A"), Arrays.asList("C", "B", "A", "B", "A", "C"))
+        assertNotNull(patch)
+        assertEquals(4, patch.deltas.size.toLong())
+        assertEquals("Patch{deltas=[[DeleteDelta, position: 0, lines: [A, B]], [InsertDelta, position: 3, lines: [B]], [DeleteDelta, position: 5, lines: [B]], [InsertDelta, position: 7, lines: [C]]]}", patch.toString())
+    }
+
+    companion object {
+
+        @Throws(IOException::class)
+        fun readStringListFromInputStream(`is`: InputStream): List<String> {
+            BufferedReader(
+                    InputStreamReader(`is`, Charset.forName(StandardCharsets.UTF_8.name()))).use { reader ->
+
+                return reader.lines().collect(toList<String>())
+            }
+        }
+    }
+}
---
+++ src/jvmTest/kotlin/dev/gitlive/difflib/text/DiffRowGeneratorTest.kt
@@ -0,0 +1,427 @@
+package dev.gitlive.difflib.text
+
+import dev.gitlive.difflib.algorithm.DiffException
+import java.io.File
+import java.io.IOException
+import java.nio.file.Files
+import java.util.Arrays
+import java.util.Collections
+import java.util.regex.Pattern
+import java.util.stream.Collectors.toList
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DiffRowGeneratorTest {
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGenerator_Default() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .columnWidth(Integer.MAX_VALUE) // do not wrap
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+    }
+
+    /**
+     * Test of normalize method, of class StringUtils.
+     */
+    @Test
+    fun testNormalize_List() {
+        val generator = DiffRowGenerator.create()
+                .build()
+        assertEquals(listOf<String>("    test"), generator.normalizeLines(listOf<String>("\ttest")))
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGenerator_Default2() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .columnWidth(0) // do not wrap
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGenerator_InlineDiff() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .columnWidth(Integer.MAX_VALUE) // do not wrap
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+        assertTrue(rows[0].oldLine!!.indexOf("<span") > 0)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGenerator_IgnoreWhitespaces() {
+        val first = "anything \n \nother\nmore lines"
+        val second = "anything\n\nother\nsome more lines"
+
+        val generator = DiffRowGenerator.create()
+                .ignoreWhiteSpaces(true)
+                .columnWidth(Integer.MAX_VALUE) // do not wrap
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(4, rows.size.toLong())
+        assertEquals(rows[0].tag, DiffRow.Tag.EQUAL)
+        assertEquals(rows[1].tag, DiffRow.Tag.EQUAL)
+        assertEquals(rows[2].tag, DiffRow.Tag.EQUAL)
+        assertEquals(rows[3].tag, DiffRow.Tag.CHANGE)
+    }
+
+    private fun split(content: String): List<String> {
+        return Arrays.asList(*content.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray())
+    }
+
+    private fun print(diffRows: List<DiffRow>) {
+        for (row in diffRows) {
+            println(row)
+        }
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithWordWrap() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .columnWidth(5)
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+        assertEquals("[CHANGE,anyth<br/>ing ,anyth<br/>ing]", rows[0].toString())
+        assertEquals("[CHANGE, ,]", rows[1].toString())
+        assertEquals("[EQUAL,other,other]", rows[2].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithMerge() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+        assertEquals("[CHANGE,anything<span class=\"editOldInline\"> </span>,anything]", rows[0].toString())
+        assertEquals("[CHANGE,<span class=\"editOldInline\"> </span>,]", rows[1].toString())
+        assertEquals("[EQUAL,other,other]", rows[2].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithMerge2() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .build()
+        val rows = generator.generateDiffRows(Arrays.asList("Test"), Arrays.asList("ester"))
+        print(rows)
+
+        assertEquals(1, rows.size.toLong())
+        assertEquals("[CHANGE,<span class=\"editOldInline\">T</span>est<span class=\"editNewInline\">er</span>,ester]", rows[0].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithMerge3() {
+        val first = "test\nanything \n \nother"
+        val second = "anything\n\nother\ntest\ntest2"
+
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(6, rows.size.toLong())
+        assertEquals("[CHANGE,<span class=\"editOldInline\">test</span>,anything]", rows[0].toString())
+        assertEquals("[CHANGE,anything<span class=\"editOldInline\"> </span>,]", rows[1].toString())
+        assertEquals("[CHANGE,<span class=\"editOldInline\"> </span>,]", rows[2].toString())
+        assertEquals("[EQUAL,other,other]", rows[3].toString())
+        assertEquals("[INSERT,<span class=\"editNewInline\">test</span>,test]", rows[4].toString())
+        assertEquals("[INSERT,<span class=\"editNewInline\">test2</span>,test2]", rows[5].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithMergeByWord4() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .inlineDiffByWord(true)
+                .build()
+        val rows = generator.generateDiffRows(Arrays.asList("Test"), Arrays.asList("ester"))
+        print(rows)
+
+        assertEquals(1, rows.size.toLong())
+        assertEquals("[CHANGE,<span class=\"editOldInline\">Test</span><span class=\"editNewInline\">ester</span>,ester]", rows[0].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorWithMergeByWord5() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .inlineDiffByWord(true)
+                .columnWidth(80)
+                .build()
+        val rows = generator.generateDiffRows(Arrays.asList("Test feature"), Arrays.asList("ester feature best"))
+        print(rows)
+
+        assertEquals(1, rows.size.toLong())
+        assertEquals("[CHANGE,<span class=\"editOldInline\">Test</span><span class=\"editNewInline\">ester</span> <br/>feature<span class=\"editNewInline\"> best</span>,ester feature best]", rows[0].toString())
+    }
+
+    @Test
+    fun testSplitString() {
+        val list = DiffRowGenerator.splitStringPreserveDelimiter("test,test2", DiffRowGenerator.SPLIT_BY_WORD_PATTERN)
+        assertEquals(3, list.size.toLong())
+        assertEquals("[test, ,, test2]", list.toString())
+    }
+
+    @Test
+    fun testSplitString2() {
+        val list = DiffRowGenerator.splitStringPreserveDelimiter("test , test2", DiffRowGenerator.SPLIT_BY_WORD_PATTERN)
+        println(list)
+        assertEquals(5, list.size.toLong())
+        assertEquals("[test,  , ,,  , test2]", list.toString())
+    }
+
+    @Test
+    fun testSplitString3() {
+        val list = DiffRowGenerator.splitStringPreserveDelimiter("test,test2,", DiffRowGenerator.SPLIT_BY_WORD_PATTERN)
+        println(list)
+        assertEquals(4, list.size.toLong())
+        assertEquals("[test, ,, test2, ,]", list.toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorExample1() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .inlineDiffByWord(true)
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val rows = generator.generateDiffRows(
+                Arrays.asList("This is a test senctence."),
+                Arrays.asList("This is a test for diffutils."))
+
+        println(rows[0].oldLine)
+
+        assertEquals(1, rows.size.toLong())
+        assertEquals("This is a test ~senctence~**for diffutils**.", rows[0].oldLine)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorExample2() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(true)
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val rows = generator.generateDiffRows(
+                Arrays.asList("This is a test senctence.", "This is the second line.", "And here is the finish."),
+                Arrays.asList("This is a test for diffutils.", "This is the second line."))
+
+        println("|original|new|")
+        println("|--------|---|")
+        for (row in rows) {
+            println("|" + row.oldLine + "|" + row.newLine + "|")
+        }
+
+        assertEquals(3, rows.size.toLong())
+        assertEquals("This is a test ~senctence~.", rows[0].oldLine)
+        assertEquals("This is a test **for diffutils**.", rows[0].newLine)
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorUnchanged() {
+        val first = "anything \n \nother"
+        val second = "anything\n\nother"
+
+        val generator = DiffRowGenerator.create()
+                .columnWidth(5)
+                .reportLinesUnchanged(true)
+                .build()
+        val rows = generator.generateDiffRows(split(first), split(second))
+        print(rows)
+
+        assertEquals(3, rows.size.toLong())
+        assertEquals("[CHANGE,anything ,anything]", rows[0].toString())
+        assertEquals("[CHANGE, ,]", rows[1].toString())
+        assertEquals("[EQUAL,other,other]", rows[2].toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue14() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .mergeOriginalRevised(true)
+                .inlineDiffBySplitter { line -> DiffRowGenerator.splitStringPreserveDelimiter(line, Regex(",")) }
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val rows = generator.generateDiffRows(
+                Arrays.asList("J. G. Feldstein, Chair"),
+                Arrays.asList("T. P. Pastor, Chair"))
+
+        println(rows[0].oldLine)
+
+        assertEquals(1, rows.size.toLong())
+        assertEquals("~J. G. Feldstein~**T. P. Pastor**, Chair", rows[0].oldLine)
+    }
+
+    @Test
+    @Throws(DiffException::class, IOException::class)
+    fun testGeneratorIssue15() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true) //show the ~ ~ and ** ** symbols on each difference
+                .inlineDiffByWord(true) //show the ~ ~ and ** ** around each different word instead of each letter
+                //.reportLinesUnchanged(true) //experiment
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+
+        val listOne = Files.lines(File("target/test-classes/mocks/issue15_1.txt").toPath())
+                .collect(toList())
+
+        val listTwo = Files.lines(File("target/test-classes/mocks/issue15_2.txt").toPath())
+                .collect(toList())
+
+        val rows = generator.generateDiffRows(listOne, listTwo)
+
+        assertEquals(9, rows.size.toLong())
+
+        for (row in rows) {
+            println("|" + row.oldLine + "| " + row.newLine + " |")
+            if (!row.oldLine!!.startsWith("TABLE_NAME")) {
+                assertTrue(row.newLine!!.startsWith("**ACTIONS_C16913**"))
+                assertTrue(row.oldLine!!.startsWith("~ACTIONS_C1700"))
+            }
+        }
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue22() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(true)
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val aa = "This is a test senctence."
+        val bb = "This is a test for diffutils.\nThis is the second line."
+        val rows = generator.generateDiffRows(
+                Arrays.asList(*aa.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()),
+                Arrays.asList(*bb.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()))
+
+        assertEquals("[[CHANGE,This is a test ~senctence~.,This is a test **for diffutils**.], [CHANGE,,**This is the second line.**]]",
+                rows.toString())
+
+        println("|original|new|")
+        println("|--------|---|")
+        for (row in rows) {
+            println("|" + row.oldLine + "|" + row.newLine + "|")
+        }
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue22_2() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(true)
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val aa = "This is a test for diffutils.\nThis is the second line."
+        val bb = "This is a test senctence."
+        val rows = generator.generateDiffRows(
+                Arrays.asList(*aa.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()),
+                Arrays.asList(*bb.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()))
+
+        assertEquals("[[CHANGE,This is a test ~for diffutils~.,This is a test **senctence**.], [CHANGE,~This is the second line.~,]]",
+                rows.toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue22_3() {
+        val generator = DiffRowGenerator.create()
+                .showInlineDiffs(true)
+                .inlineDiffByWord(true)
+                .oldTag { f -> "~" }
+                .newTag { f -> "**" }
+                .build()
+        val aa = "This is a test senctence."
+        val bb = "This is a test for diffutils.\nThis is the second line.\nAnd one more."
+        val rows = generator.generateDiffRows(
+                Arrays.asList(*aa.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()),
+                Arrays.asList(*bb.split("\n".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()))
+
+        assertEquals("[[CHANGE,This is a test ~senctence~.,This is a test **for diffutils**.], [CHANGE,,**This is the second line.**], [CHANGE,,**And one more.**]]",
+                rows.toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue41DefaultNormalizer() {
+        val generator = DiffRowGenerator.create()
+                .build()
+        val rows = generator.generateDiffRows(Arrays.asList("<"), Arrays.asList("<"))
+        assertEquals("[[EQUAL,&lt;,&lt;]]", rows.toString())
+    }
+
+    @Test
+    @Throws(DiffException::class)
+    fun testGeneratorIssue41UserNormalizer() {
+        val generator = DiffRowGenerator.create()
+                .lineNormalizer { str -> str.replace("\t", "    ") }
+                .build()
+        var rows = generator.generateDiffRows(Arrays.asList("<"), Arrays.asList("<"))
+        assertEquals("[[EQUAL,<,<]]", rows.toString())
+        rows = generator.generateDiffRows(Arrays.asList("\t<"), Arrays.asList("<"))
+        assertEquals("[[CHANGE,    <,<]]", rows.toString())
+    }
+}
