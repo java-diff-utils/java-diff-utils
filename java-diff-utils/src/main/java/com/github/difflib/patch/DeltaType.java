f76abb58f6e022dc58ddb1805f1ce980996917d3
Tobias Warneke <t.warneke@gmx.net>
2020-09-27 18:13:03 +0200

    improved the documentation of DeltyType a bit fixed #99

--- java-diff-utils/src/main/java/com/github/difflib/patch/DeltaType.java
+++ java-diff-utils/src/main/java/com/github/difflib/patch/DeltaType.java
@@ -16,7 +16,18 @@
 package com.github.difflib.patch;
 
 /**
- * Specifies the type of the delta.
+ * Specifies the type of the delta. There are three types of modifications from  
+ * the original to get the revised text. 
+ * 
+ * CHANGE: a block of data of the original is replaced by another block of data.
+ * DELETE: a block of data of the original is removed
+ * INSERT: at a position of the original a block of data is inserted
+ * 
+ * to be complete there is also 
+ * 
+ * EQUAL: a block of data of original and the revised text is equal
+ * 
+ * which is no change at all.
  *
  */
 public enum DeltaType {
