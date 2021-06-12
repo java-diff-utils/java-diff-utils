c1c0a98474715b5f0f50558605e3d2e05caa4d19
anatawa12 <anatawa12@icloud.com>
2021-06-13 08:53:18 +0900

    fix use explict import

--- java-diff-utils/src/test/java/com/github/difflib/patch/ChunkTest.java
+++ java-diff-utils/src/test/java/com/github/difflib/patch/ChunkTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChunkTest {
     @Test
