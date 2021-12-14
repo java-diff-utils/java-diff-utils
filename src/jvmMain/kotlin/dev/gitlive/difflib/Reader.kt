766af91fa431968967470d4fa7bbf49224e9498f
Michael Richardson <michael@richardsons.co.za>
2021-12-14 17:51:31 +0000

    Remove Reader

--- src/jvmMain/kotlin/dev/gitlive/difflib/Reader.kt
+++
@@ -1,8 +0,0 @@
-package dev.gitlive.difflib
-
-
-actual typealias Reader = java.io.Reader
-
-actual typealias BufferedReader = java.io.BufferedReader
-actual typealias InputStreamReader = java.io.InputStreamReader
-actual typealias InputStream = java.io.InputStream
